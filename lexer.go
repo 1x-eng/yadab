347c8eb6c04e9dd175b7240fcedffd1f13382b5b
Pruthvi Kumar <pruthvikumar.123@gmail.com>
2023-07-24 20:19:29 +1000

    feat: lexer for integer & string

--- lexer.go
+++ lexer.go
@@ -1,5 +1,7 @@
 package yadab
 
+import "fmt"
+
 type location struct {
 	line uint
 	col  uint
@@ -56,3 +58,180 @@ func (t *token) equals(other *token) bool {
 }
 
 type lexer func(string, cursor) (*token, cursor, bool)
+
+// lexNumeric is a Go function that takes a source string and a cursor as input
+// and returns a token, a cursor, and a boolean value indicating success or
+// failure. It lexes the numeric portion of the source string.
+//
+// Parameters:
+//   - source: the source string to lex
+//   - ic: the initial cursor position
+//
+// Returns:
+//   - token: the lexed token
+//   - cursor: the updated cursor position
+//   - bool: indicating success or failure
+func lexNumeric(source string, ic cursor) (*token, cursor, bool) {
+	cur := ic
+
+	periodFound := false
+	expMarkerFound := false
+
+	for ; cur.pointer < uint(len(source)); cur.pointer++ {
+		c := source[cur.pointer]
+		cur.loc.col++
+
+		isDigit := c >= '0' && c <= '9'
+		isPeriod := c == '.'
+		isExpMarker := c == 'e'
+
+		// Must start with a digit or period
+		if cur.pointer == ic.pointer {
+			if !isDigit && !isPeriod {
+				return nil, ic, false
+			}
+
+			periodFound = isPeriod
+			continue
+		}
+
+		if isPeriod {
+			if periodFound {
+				return nil, ic, false
+			}
+
+			periodFound = true
+			continue
+		}
+
+		if isExpMarker {
+			if expMarkerFound {
+				return nil, ic, false
+			}
+
+			// No periods allowed after expMarker
+			periodFound = true
+			expMarkerFound = true
+
+			// expMarker must be followed by digits
+			if cur.pointer == uint(len(source)-1) {
+				return nil, ic, false
+			}
+
+			cNext := source[cur.pointer+1]
+			if cNext == '-' || cNext == '+' {
+				cur.pointer++
+				cur.loc.col++
+			}
+
+			continue
+		}
+
+		if !isDigit {
+			break
+		}
+	}
+
+	// No characters accumulated
+	if cur.pointer == ic.pointer {
+		return nil, ic, false
+	}
+
+	return &token{
+		value: source[ic.pointer:cur.pointer],
+		loc:   ic.loc,
+		kind:  numericKind,
+	}, cur, true
+}
+
+// lexCharacterDelimited lexes a character-delimited token from the given source string starting at the specified cursor position.
+//
+// Parameters:
+// - source: The source string to lex from.
+// - ic: The initial cursor position.
+// - delimiter: The delimiter character to use.
+//
+// Returns:
+// - token: The lexed token, or nil if no token could be lexed.
+// - cursor: The updated cursor position.
+// - bool: A boolean indicating whether a token was lexed.
+func lexCharacterDelimited(source string, ic cursor, delimiter byte) (*token, cursor, bool) {
+	cur := ic
+
+	if len(source[cur.pointer:]) == 0 {
+		return nil, ic, false
+	}
+
+	if source[cur.pointer] != delimiter {
+		return nil, ic, false
+	}
+
+	cur.loc.col++
+	cur.pointer++
+
+	var value []byte
+	for ; cur.pointer < uint(len(source)); cur.pointer++ {
+		c := source[cur.pointer]
+
+		if c == delimiter {
+			// SQL escapes are via double characters, not backslash.
+			if cur.pointer+1 >= uint(len(source)) || source[cur.pointer+1] != delimiter {
+				return &token{
+					value: string(value),
+					loc:   ic.loc,
+					kind:  stringKind,
+				}, cur, true
+			} else {
+				value = append(value, delimiter)
+				cur.pointer++
+				cur.loc.col++
+			}
+		}
+
+		value = append(value, c)
+		cur.loc.col++
+	}
+
+	return nil, ic, false
+}
+
+// lexString is a function that lexes a string literal in the given source code.
+//
+// It takes the source code as a string and the initial cursor position as input.
+// It returns a token, a new cursor position, and a boolean value indicating whether lexing was successful.
+func lexString(source string, ic cursor) (*token, cursor, bool) {
+	return lexCharacterDelimited(source, ic, '\'')
+}
+
+func lex(source string) ([]*token, error) {
+	tokens := []*token{}
+	cur := cursor{}
+
+lex:
+	for cur.pointer < uint(len(source)) {
+		lexers := []lexer{lexString, lexNumeric}
+		for _, l := range lexers {
+			if token, newCursor, ok := l(source, cur); ok {
+				cur = newCursor
+
+				// Omit nil tokens for valid, but empty syntax like newlines
+				if token != nil {
+					tokens = append(tokens, token)
+				}
+
+				continue lex
+			}
+		}
+
+		hint := ""
+		if len(tokens) > 0 {
+			hint = " after " + tokens[len(tokens)-1].value
+		}
+		for _, t := range tokens {
+			fmt.Println(t.Value)
+		}
+		return nil, fmt.Errorf("Unable to lex token%s, at %d:%d", hint, cur.loc.line, cur.loc.col)
+	}
+
+	return tokens, nil
+}
