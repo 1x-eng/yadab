3ac1838b48419ad65d8a63ba504fc8e9bb874084
Pruthvi Kumar <pruthvikumar.123@gmail.com>
2023-07-24 20:27:53 +1000

    feat: lexer for keywords, identifiers

--- lexer.go
+++ lexer.go
@@ -1,6 +1,9 @@
 package yadab
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type location struct {
 	line uint
@@ -20,6 +23,7 @@ const (
 	valuesKeyword keyword = "values"
 	intKeyword    keyword = "int"
 	textKeyword   keyword = "text"
+	whereKeyword  keyword = "where"
 )
 
 type symbol string
@@ -28,8 +32,17 @@ const (
 	semicolonSymbol  symbol = ";"
 	asteriskSymbol   symbol = "*"
 	commaSymbol      symbol = ","
-	leftparenSymbol  symbol = "("
-	rightparenSymbol symbol = ")"
+	leftParenSymbol  symbol = "("
+	rightParenSymbol symbol = ")"
+	eqSymbol         symbol = "="
+	neqSymbol        symbol = "<>"
+	neqSymbol2       symbol = "!="
+	concatSymbol     symbol = "||"
+	plusSymbol       symbol = "+"
+	ltSymbol         symbol = "<"
+	lteSymbol        symbol = "<="
+	gtSymbol         symbol = ">"
+	gteSymbol        symbol = ">="
 )
 
 type tokenKind uint
@@ -201,6 +214,219 @@ func lexCharacterDelimited(source string, ic cursor, delimiter byte) (*token, cu
 // It returns a token, a new cursor position, and a boolean value indicating whether lexing was successful.
 func lexString(source string, ic cursor) (*token, cursor, bool) {
 	return lexCharacterDelimited(source, ic, '\'')
+}
+
+// lexSymbol is a Go function that performs lexical analysis on a given source string and cursor position.
+//
+// Parameters:
+// - source: the source string to analyze.
+// - ic: the initial cursor position.
+//
+// Returns:
+// - token: the token found during analysis.
+// - cursor: the updated cursor position.
+// - bool: a boolean indicating whether the token is valid or not.
+func lexSymbol(source string, ic cursor) (*token, cursor, bool) {
+	c := source[ic.pointer]
+	cur := ic
+	// Will get overwritten later if not an ignored syntax
+	cur.pointer++
+	cur.loc.col++
+
+	switch c {
+	// Syntax that should be thrown away
+	case '\n':
+		cur.loc.line++
+		cur.loc.col = 0
+		fallthrough
+	case '\t':
+		fallthrough
+	case ' ':
+		return nil, cur, true
+	}
+
+	// Syntax that should be kept
+	symbols := []symbol{
+		commaSymbol,
+		leftParenSymbol,
+		rightParenSymbol,
+		semicolonSymbol,
+		asteriskSymbol,
+	}
+
+	var options []string
+	for _, s := range symbols {
+		options = append(options, string(s))
+	}
+
+	// Use `ic`, not `cur`
+	match := longestMatch(source, ic, options)
+	// Unknown character
+	if match == "" {
+		return nil, ic, false
+	}
+
+	cur.pointer = ic.pointer + uint(len(match))
+	cur.loc.col = ic.loc.col + uint(len(match))
+
+	return &token{
+		value: match,
+		loc:   ic.loc,
+		kind:  symbolKind,
+	}, cur, true
+}
+
+// lexKeyword lexes a keyword from the source string starting at the given cursor position.
+//
+// Parameters:
+// - source: The source string to lex.
+// - ic: The initial cursor position.
+//
+// Returns:
+// - token: The lexed token.
+// - cursor: The updated cursor position.
+// - bool: Indicates if a keyword was successfully lexed.
+func lexKeyword(source string, ic cursor) (*token, cursor, bool) {
+	cur := ic
+	keywords := []keyword{
+		selectKeyword,
+		insertKeyword,
+		valuesKeyword,
+		tableKeyword,
+		createKeyword,
+		whereKeyword,
+		fromKeyword,
+		intoKeyword,
+		textKeyword,
+	}
+
+	var options []string
+	for _, k := range keywords {
+		options = append(options, string(k))
+	}
+
+	match := longestMatch(source, ic, options)
+	if match == "" {
+		return nil, ic, false
+	}
+
+	cur.pointer = ic.pointer + uint(len(match))
+	cur.loc.col = ic.loc.col + uint(len(match))
+
+	return &token{
+		value: match,
+		kind:  kind,
+		loc:   ic.loc,
+	}, cur, true
+}
+
+// longestMatch finds the longest match in a given source string, based on a list of options.
+//
+// Parameters:
+// - source: the source string to search within.
+// - ic: the initial cursor position.
+// - options: a list of strings representing the options to match against.
+//
+// Returns:
+// - string: the longest match found from the options list.
+func longestMatch(source string, ic cursor, options []string) string {
+	var value []byte
+	var skipList []int
+	var match string
+
+	cur := ic
+
+	for cur.pointer < uint(len(source)) {
+
+		value = append(value, strings.ToLower(string(source[cur.pointer]))...)
+		cur.pointer++
+
+	match:
+		for i, option := range options {
+			for _, skip := range skipList {
+				if i == skip {
+					continue match
+				}
+			}
+
+			// Deal with cases like INT vs INTO
+			if option == string(value) {
+				skipList = append(skipList, i)
+				if len(option) > len(match) {
+					match = option
+				}
+
+				continue
+			}
+
+			sharesPrefix := string(value) == option[:cur.pointer-ic.pointer]
+			tooLong := len(value) > len(option)
+			if tooLong || !sharesPrefix {
+				skipList = append(skipList, i)
+			}
+		}
+
+		if len(skipList) == len(options) {
+			break
+		}
+	}
+
+	return match
+}
+
+// lexIdentifier is a function that lexes an identifier from the given source string at the specified cursor position.
+//
+// Parameters:
+// - source: The source string to lex from.
+// - ic: The cursor position in the source string.
+//
+// Returns:
+// - token: The lexed token representing the identifier.
+// - cursor: The updated cursor position after lexing the identifier.
+// - ok: A boolean indicating whether the lexing was successful.
+func lexIdentifier(source string, ic cursor) (*token, cursor, bool) {
+	// Handle separately if is a double-quoted identifier
+	if token, newCursor, ok := lexCharacterDelimited(source, ic, '"'); ok {
+		return token, newCursor, true
+	}
+
+	cur := ic
+
+	c := source[cur.pointer]
+	// Other characters count too, big ignoring non-ascii for now
+	isAlphabetical := (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')
+	if !isAlphabetical {
+		return nil, ic, false
+	}
+	cur.pointer++
+	cur.loc.col++
+
+	value := []byte{c}
+	for ; cur.pointer < uint(len(source)); cur.pointer++ {
+		c = source[cur.pointer]
+
+		// Other characters count too, big ignoring non-ascii for now
+		isAlphabetical := (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')
+		isNumeric := c >= '0' && c <= '9'
+		if isAlphabetical || isNumeric || c == '$' || c == '_' {
+			value = append(value, c)
+			cur.loc.col++
+			continue
+		}
+
+		break
+	}
+
+	if len(value) == 0 {
+		return nil, ic, false
+	}
+
+	return &token{
+		// Unquoted dentifiers are case-insensitive
+		value: strings.ToLower(string(value)),
+		loc:   ic.loc,
+		kind:  identifierKind,
+	}, cur, true
 }
 
 func lex(source string) ([]*token, error) {
