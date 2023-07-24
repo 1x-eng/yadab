8e3c1841970b81ddadd52a4a3d1175b767275c0f
Pruthvi Kumar <pruthvikumar.123@gmail.com>
2023-07-24 19:50:49 +1000

    feat: lexer - wip

---
+++ lexer.go
@@ -0,0 +1,58 @@
+package yadab
+
+type location struct {
+	line uint
+	col  uint
+}
+
+type keyword string
+
+const (
+	selectKeyword keyword = "select"
+	fromKeyword   keyword = "from"
+	asKeyword     keyword = "as"
+	tableKeyword  keyword = "table"
+	createKeyword keyword = "create"
+	insertKeyword keyword = "insert"
+	intoKeyword   keyword = "into"
+	valuesKeyword keyword = "values"
+	intKeyword    keyword = "int"
+	textKeyword   keyword = "text"
+)
+
+type symbol string
+
+const (
+	semicolonSymbol  symbol = ";"
+	asteriskSymbol   symbol = "*"
+	commaSymbol      symbol = ","
+	leftparenSymbol  symbol = "("
+	rightparenSymbol symbol = ")"
+)
+
+type tokenKind uint
+
+const (
+	keywordKind tokenKind = iota
+	symbolKind
+	identifierKind
+	stringKind
+	numericKind
+)
+
+type token struct {
+	value string
+	kind  tokenKind
+	loc   location
+}
+
+type cursor struct {
+	pointer uint
+	loc     location
+}
+
+func (t *token) equals(other *token) bool {
+	return t.value == other.value && t.kind == other.kind
+}
+
+type lexer func(string, cursor) (*token, cursor, bool)
